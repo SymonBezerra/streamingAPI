9d2c93af69cd4a3a52cf45ab93eb5db8884b5731
SymonBezerra <simaobezerratranslate@gmail.com>
2022-12-28 10:54:11 -0300

    added MovieDTO

---
+++ streaming/src/main/java/ifpb/edu/br/streaming/dto/MovieDTO.java
@@ -0,0 +1,15 @@
+package ifpb.edu.br.streaming.dto;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MovieDTO {
+    
+    private String name;
+    private String category;
+    private List<String> tags;
+}
