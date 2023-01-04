ed7e35eb05f3009221206611dff9a4d10d24415b
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-04 09:57:06 -0300

    listByName impl

--- streaming/src/main/java/ifpb/edu/br/streaming/controller/MovieController.java
+++ streaming/src/main/java/ifpb/edu/br/streaming/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ifpb.edu.br.streaming.domain.Movie;
@@ -49,4 +50,18 @@ public class MovieController {
             return ResponseEntity.badRequest().body(new ErrorDTO(ex.getMessage()));
         }
     }
+
+    @GetMapping("/name")
+    public ResponseEntity <?> listByName (@RequestParam String name) {
+
+        try {
+            Movie movie = movieService.findByName(name);
+            return ResponseEntity.ok(movieMapper.convertToDTO(movie));
+        } catch (ContentNotFoundException ex) {
+            return ResponseEntity.badRequest().body(new ErrorDTO(ex.getMessage()));
+        }
+    }
+
+    
+
 }
