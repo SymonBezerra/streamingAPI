28f6579e2e6178f4089a68f1a8b4a20847163713
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-16 10:35:55 -0300

    Swagger 3.0.0 -> 2.9.2

---
+++ streaming/src/main/java/ifpb/edu/br/streaming/swagger/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package ifpb.edu.br.streaming.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket
+
+}
