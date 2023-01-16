c339698ae57a2bf37632d026b583ceb42591c395
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-16 11:01:34 -0300

    Swagger3 succesfully impl

--- streaming/src/main/java/ifpb/edu/br/streaming/swagger/SwaggerConfig.java
+++ streaming/src/main/java/ifpb/edu/br/streaming/swagger/SwaggerConfig.java
@@ -10,32 +10,25 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
-    
+
     @Bean
-    public Docket productApi () {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-        .apis(RequestHandlerSelectors.basePackage("ifpb.edu.br.streaming"))
-        .build().apiInfo(metaData());
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("br.edu.uepb.coffee"))
+                .build().apiInfo(metaData());
     }
 
-    private ApiInfo metaData () {
-        return new ApiInfoBuilder().title("Streaming API - Grupo de Estudos | Prof. Henrique Cunha")
-        .version("1.0.0").license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0/")
-        .build();
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder().title("Coffee Restful API").description("\"Supported by Spring Boot\"")
+                .version("1.0.0").license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"").build();
     }
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
-        
-        registry.addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
 }
