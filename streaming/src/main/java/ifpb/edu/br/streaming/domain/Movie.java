3f868ae432919e200d47a3dd251ee0c374d4a002
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-16 12:33:40 -0300

    -

--- streaming/src/main/java/ifpb/edu/br/streaming/domain/Movie.java
+++ streaming/src/main/java/ifpb/edu/br/streaming/domain/Movie.java
@@ -2,17 +2,11 @@ package ifpb.edu.br.streaming.domain;
 
 
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -40,6 +34,7 @@ public class Movie {
     // details
     @ElementCollection
     private List<String> movieCast;
+    
     private int duration;
     private String description;
 }
